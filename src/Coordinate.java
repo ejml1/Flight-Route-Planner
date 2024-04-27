2ca9b3885a30e7af2d566c746d4a978989b60410
ejml1 <ejml1@st-andrews.ac.uk>
2024-04-27 15:34:31 +0100

    Update Coordinate to use TreeSet for Neighbours

--- src/Coordinate.java
+++ src/Coordinate.java
@@ -1,23 +1,52 @@
-import java.util.ArrayList;
+import java.util.TreeSet;
 
-public class Coordinate {
+public class Coordinate implements Comparable<Coordinate>{
     
     private int parallel;
     private int angle;
-    private ArrayList<Coordinate> neighbours;
+    private TreeSet<Coordinate> neighbours;
 
     public Coordinate(int parallel, int angle) {
         this.parallel = parallel;
         this.angle = angle;
-        this.neighbours = new ArrayList<Coordinate>();
+        this.neighbours = new TreeSet<Coordinate>();
     }
 
     public void addNeighbour(Coordinate neighbour) {
         this.neighbours.add(neighbour);
     }
 
-    public ArrayList<Coordinate> getNeighbours() {
+    public TreeSet<Coordinate> getNeighbours() {
         return this.neighbours;
+    }
+
+    public int getParallel() {
+        return this.parallel;
+    }
+
+    public void setParallel(int parallel) {
+        this.parallel = parallel;
+    }
+
+    public int getAngle() {
+        return this.angle;
+    }
+
+    public void setAngle(int angle) {
+        this.angle = angle;
+    }
+    public void setNeighbours(TreeSet<Coordinate> neighbours) {
+        this.neighbours = neighbours;
+    }
+
+    @Override
+    public int compareTo(Coordinate other) {
+        int parallelComparison = Integer.compare(this.parallel, other.parallel);
+        if (parallelComparison != 0) {
+            return parallelComparison;
+        }
+
+        return Integer.compare(this.angle, other.angle);
     }
 
     public String toString() {
